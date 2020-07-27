e4af77c4a66522c3551e7093ea0b859c41dce3c3
flareg255 <flare.g255@gmail.com>
2020-07-27 14:52:07 +0900

    画像保存とExcel化

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Url_str, time, urllib, os
+import Url_str, time, urllib, os, glob, openpyxl, PIL
 from pyquery import PyQuery as pq
 
 class Main:
@@ -7,7 +7,16 @@ class Main:
     urls = url_str.getUrls()
 
     def getImg(self):
-        resultItems = []
+        cnt = 1
+        wb = None
+
+        if len(glob.glob(self.url_str.excelDataPath)) == 0:
+            wb = openpyxl.Workbook()
+            sheet = wb.active
+            sheet.title = 'data_sheet_1'
+            wb.save(self.url_str.excelDataPath)
+        else:
+            wb = openpyxl.load_workbook(self.url_str.excelDataPath)
 
         for url in self.urls:
             time.sleep(3)
@@ -15,16 +24,35 @@ class Main:
             data = pq(url)
             data = data(self.url_str.itemGroup)('li')('a')('img')
 
+            sheet = wb['data_sheet_1']
+            sheet.column_dimensions['A'].width = self.url_str.colAWidth
+            sheet.column_dimensions['B'].width = self.url_str.colBWidth
+
             for img in data:
                 resultItem = {}
 
                 if not pq(img).attr('src') == 'None':
                     resultItem['alt'] = pq(img).attr('alt')
                     resultItem['img'] = pq(img).attr('src')
-                    urllib.request.urlretrieve(resultItem['img'], os.path.join('../img/', resultItem['img'].replace(self.url_str.delImgPath, '')))
-                    resultItems.append(resultItem)
+                    print(resultItem)
 
-        return resultItems
+                    imgPath = os.path.join('../img/', resultItem['img'].replace(self.url_str.delImgPath, ''))
+
+                    try:
+                        urllib.request.urlretrieve(resultItem['img'], imgPath)
+                        imgItem = openpyxl.drawing.image.Image(imgPath)
+                        sheet.add_image( imgItem, 'b' + str(cnt) )
+                        sheet.row_dimensions[cnt].height = self.url_str.rowHeight
+                    except BaseException as e:
+                        print(resultItem['img'])
+                        print(e)
+                    finally:
+                        sheet['a' + str(cnt)] = pq(img).attr('alt')
+                        sheet['c' + str(cnt)] = pq(img).attr('src')
+                        wb.save(self.url_str.excelDataPath)
+
+                        cnt+=1
+
 
 main = Main()
-print(main.getImg())
+main.getImg()
